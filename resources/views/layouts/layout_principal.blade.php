9627cfa4c2efc192376a42b54fd563e076d90ea9
Rodren1 <bermejorodriguezz@gmail.com>
2020-10-22 21:28:59 +0200

    controlador añadido para conectarse a la bbdd

---
+++ resources/views/layouts/layout_principal.blade.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+
+
+
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>

a0451d7e32b566c64ea093b14404f6255c52b485
Rodren1 <bermejorodriguezz@gmail.com>
2020-10-28 18:24:30 +0100

    añadida otra forma de consulta comentada

--- app/Http/Controllers/EquiposController.php
+++ app/Http/Controllers/EquiposController.php
@@ -13,6 +13,7 @@ class EquiposController extends Controller
         $equipo = DB::table('equipos') //creo la variable $equipo y la igualo a la consulta
             ->where('nombre', '=', $nombre) // donde el nombre de la bbdd es igual al parametro de entrada
             ->get();
+        // $equipo = Equipos::where('nombre', $nombre)->get(); consulta eloquent    
         return view('datosEquipo', array('equipos' => $equipo)); //retorno la vista de los datos obtenidos del equipo en cuestion
     }
 }
